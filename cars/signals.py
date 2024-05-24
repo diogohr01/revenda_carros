4ac8729602675446878ad23bfabc1bf4ddb84812
Diogo Rodrigues <diogorodrigues699@yahoo.com>
2024-05-24 11:09:52 -0300

    mudanças no signals para mostrar os valores no estoque

--- cars/signals.py
+++ cars/signals.py
@@ -5,12 +5,13 @@ from cars.models import Car, CarInventory
 
 
 def car_inventory_update():
-    cars_count = Car.objects.all().count()
-    cars_value = Car.objects.aggregate(total_value = Sum('value'))['total_value']
+    cars_count = Car.objects.all().count() or 0
+    cars_value = Car.objects.aggregate(total_value = Sum('value'))['total_value'] or 0
     CarInventory.objects.create(
         cars_count=cars_count,
         cars_value=cars_value
-    )
+        )
+        
     
 @receiver(post_save, sender=Car)
 def car_post_save(sender, instance, **kwargs):
